bb8f45af8c00488af6c65c4aa662a96132b4094d
Athu5 <84505685+Athu5@users.noreply.github.com>
2021-06-07 22:57:03 +0530

    Add files via upload

---
+++ pro 20/tomAndJerryTemplate-master/sketch.js
@@ -0,0 +1,63 @@
+var cat, catImg1, catImg2, catImg3, mouse, mouseImg1, mouseImg2, mouseImg3 ;
+var back;
+
+function preload() {
+    //load the images here
+catImg1 = loadImage("images/cat1.png");
+catImg2 = loadAnimation("images/cat2.png", "images/cat3.png");
+catImg3 = loadImage("images/cat4.png");
+mouseImg1 = loadImage("images/mouse1.png");
+mouseImg2 = loadAnimation("images/mouse2.png", "images/mouse3.png");
+mouseImg3 = loadImage("images/mouse4.png");
+back = loadImage("images/garden.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+   cat = createSprite(800, 650);
+   cat.addAnimation("sleep", catImg1);
+   cat.scale = 0.2;
+
+   mouse = createSprite(200, 650);
+   mouse.addImage("happy", mouseImg1);
+   mouse.scale = 0.1;
+}
+
+function draw() {
+
+    background(back);
+    //Write condition here to evalute if tom and jerry collide
+
+    if(cat.x - mouse.x <(cat.width - mouse.width)/2) {
+        cat.velocityX = 0;
+        cat.addImage("sit", catImg3);
+        cat.changeImage("sit");
+        mouse.addImage("glass", mouseImg3);
+        mouse.changeImage("glass");
+    }
+
+
+    
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+    
+
+if(keyCode === LEFT_ARROW){
+    mouse.addAnimation("teasing", mouseImg2);
+    mouse.changeAnimation("teasing");
+    mouse.frameDelay = 10;
+    cat.addAnimation("walking", catImg2);
+    cat.changeAnimation("walking");
+    cat.velocityX = -5;
+}
+
+
+}
